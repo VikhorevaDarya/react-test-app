416f49944d0919d46051a84d139521f6ff68f5cf
Darya Vikhoreva <vikhorevadv@gmail.com>
2022-04-09 15:58:22 +0700

    dev header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import '../styles/Header.css';
+import '../styles/Header.scss';
+import LogoIcon from '../svg/LogoIcon.js';
 
 function Header() {
   return (
     <div className="header">
-      
+      <div className="header__logo">
+        <LogoIcon />
+      </div>
+      <div class="header__menu">
+        <div class="header__item">
+          Features
+        </div>
+        <div class="header__item">
+          Pricing
+        </div>
+        <div class="header__item">
+          Testimonials
+        </div>
+        <div class="header__item">
+          Instructions
+        </div>
+        <div class="header__item header__button">
+          GET TEMPLATE
+        </div>
+      </div>
     </div>
   );
 }
